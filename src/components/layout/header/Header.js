4996d874522f473b4f3583ea683ecd8339b566a8
jason-smith-code <jasonsmithcode@gmail.com>
2022-04-13 15:23:37 +0100

    desktop layout almost complete

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,16 +1,22 @@
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
     return(
         <div id="header">
             <div id="logo-container">
                 <FontAwesomeIcon icon={faCircleCheck} size="lg" />
-                <p id="logo-text">TASKIFY</p>
+                <p id="logo-text">Taskify</p>
             </div>
-            {/* Menu */}
-            <p>MENU</p>
+            <div id="login-register-menu-container">
+                    <p className="spacing-right">Login / Register</p>
+                <div className="menu-container">
+                    <FontAwesomeIcon icon={faBars} size="lg" />
+                    <p className="top-menu-text">Menu</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -1,7 +1,36 @@
+import './Main.css';
+
 export function Main() {
     return(
-        <p>
-            Main Content
-        </p>
+        <div id="main">
+            <div className="column adding-column">
+                <div className="adding-column-split add-tasks">
+                    <h2>Add tasks</h2>
+                    <p>Add a button, which expands into a form to add a task. The form will simply ask for a Title and a Description</p>
+                </div>
+                <div className="adding-column-split add-categories">
+                    <h2>Add categories</h2>
+                    <p>create a button (text field) which adds a category column, the text field will be the name of the category</p>
+                </div>
+            </div>
+            <div className="column category-column pending-column">
+                <h2>Pending tasks</h2>
+                <p>All newly created tasks will go here</p>
+            </div>
+            <div className="column category-column column-1">
+                <h2>Category 1 tasks</h2>
+                <p>Tasks can be dragged and dropped into any one of these columns, to sort and organise them</p>
+                <p>Tasks can also be moved up and down each column to present a heirachi of importance</p>
+            </div>
+            <div className="column category-column column-2">
+                <h2>Category 2 tasks</h2>
+            </div>
+            <div className="column category-column column-3">
+                <h2>Category 2 tasks</h2>
+            </div>
+            <div className="column completed-column">
+                <h2>Completed tasks</h2>
+            </div>
+        </div>
     )
 }
