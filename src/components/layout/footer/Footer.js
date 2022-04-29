8b54349296daae50f8048c86f003e7dfcbb07d2d
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-04-29 14:56:36 +0100

    Optimised display for footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 export function Footer() {
     return(
         <div id="Footer">
-            <p>Designed and developed by <a href="https://github.com/Jason-Smith-Code" rel="noreferrer" target="_blank">Jason Smith</a></p>
+            <p id="foot-text">Designed and developed by <a href="https://github.com/Jason-Smith-Code" rel="noreferrer" target="_blank">Jason Smith</a></p>
         </div>
     )
 }
